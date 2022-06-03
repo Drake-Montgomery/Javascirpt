b0796b87f30b260cc557e4ca3e6741ff54f51950
Drake-Montgomery <eder_vip@outlook.com>
2022-06-03 17:34:30 -0300

    Aula 11 - ex008, ex009 e ex010

---
+++ Javascript/Aula_11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 61
+console.log(`A velocidade do seu carro ${vel}Km/h`)
+
+if(vel > 60) { //Condição Simples!
+   console.log(`MULTADO ! Você estava à ${vel}Km/h, acima do limite de velocidade permitida 60Km/h `) 
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
